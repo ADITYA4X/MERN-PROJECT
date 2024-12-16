02a78fdc9c44f75633766a3322f03c911ebe22b9
aditya <adityakunghadkar@gmail.com>
2024-12-16 16:22:16 +0530

    Art-X Frontend : Manage Project Header Design

--- Art-X/dashboard/src/layout/Header.jsx
+++ Art-X/dashboard/src/layout/Header.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import { FaList } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({ showSidebar, setShowSidebar }) => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40 bg-white">
+      <div className="ml-0 lg:ml-[310px] h-[140px] flex justify-between items-center px-5 transition-all ">
+        <div
+          onClick={() => setShowSidebar(!showSidebar)}
+          className="w-[35px] flex lg:hidden h-[35px] rounded-sm bg-transparent text-white text-2xl shadow-lg hover:bg-white hover:text-black hover:shadow-gray-300/50 justify-center items-center cursor-pointer"
+        >
+          <span>
+            <FaList />
+          </span>
+        </div>
+
+        <div className="relative hidden lg:block">
+          <div className="md:block flex justify-center items-center flex-col">
+            <h1 className="text-black text-[42px] font-semibold md:text-[36px] mb-1">
+              Welcome back,
+            </h1>
+            <span className="text-[#737374] font-semibold">
+              Here are today's stats from your Art-X store!
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-[130px]">
+          <div className="relative hidden md:block left-16">
+            <input
+              className="h-[48px] pl-14 pr-1 py-1 outline-none border bg-[#e1e0e0] rounded-full text-stone-800 text-lg font-semibold overflow-hidden focus:outline-none focus:ring-1 focus:ring-stone-400
+              focus:w-80 transition-all duration-500"
+              type="text"
+              name="search"
+              placeholder="Search"
+            />
+            <div className="absolute left-6 top-1/2 transform -translate-y-1/2 text-stone-700 text-lg">
+              <FaSearch />
+            </div>
+          </div>
+
+          <div className="flex justify-center items-center gap-8 relative text-stone-600">
+            <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center gap-3">
+                <div className="flex justify-center items-center flex-col text-end">
+                  <h1 className="text-lg font-bold">Aditya Kumar</h1>
+                  <span className="text-[#737374] font-semibold w-full">
+                    Admin
+                  </span>
+                </div>
+                <img
+                  className="w-[42px] h-[42px] rounded-full overflow-hidden"
+                  src="http://localhost:3001/images/admin.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- Art-X/dashboard/src/layout/Sidebar.jsx
+++ Art-X/dashboard/src/layout/Sidebar.jsx
@@ -6,10 +6,10 @@ import { TbLogout2 } from "react-icons/tb";
 import { MdOutlineDateRange } from "react-icons/md";
 import { TiTime } from "react-icons/ti";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
   const { pathname } = useLocation();
-
   const [allNav, setAllNav] = useState([]);
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const navs = getNav("admin");
@@ -17,28 +17,42 @@ const Sidebar = () => {
   }, []);
   // console.log(allNav);
 
-  // Create a new Date object
-  const today = new Date();
+  useEffect(() => {
+    // Update the time every second
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(timer);
+  }, []);
 
   // Format the date (e.g., 15 Dec 2024)
-  const day = today.getDate();
-  const month = today.toLocaleString("default", { month: "short" }); // Short month name (e.g., "Dec")
-  const year = today.getFullYear();
+  const day = currentTime.getDate();
+  const month = currentTime.toLocaleString("default", { month: "short" });
+  const year = currentTime.getFullYear();
 
-  let hours = today.getHours();
-  const minutes = today.getMinutes().toString().padStart(2, "0"); // Ensure two digits for minutes
+  let hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes().toString().padStart(2, "0"); // Ensure two digits for minutes
   const amPm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12 || 12;
 
   return (
     <div>
-      <div></div>
+      <div
+        onClick={() => setShowSidebar(false)}
+        className={`fixed duration-200 ${
+          !showSidebar ? "invisible" : "visible"
+        } w-screen h-screen bg-black opacity-50 top-0 left-0 z-10`}
+      ></div>
 
       <div
-        className={`w-[310px] fixed bg-[#202123] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_10%)] transition-all`}
+        className={`w-[310px] fixed bg-stone-950 z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_10%)] transition-all ${
+          showSidebar ? "left-0" : "-left-[310px] lg:left-0"
+        }`}
       >
         <div className="h-[70px] flex justify-center items-center">
-          <Link to="/" className="w-[180px] h-[50px] ml-[-92px]">
+          <Link to="/" className="w-[200px] h-[90px] ml-[-92px]">
             <img
               className="w-full"
               src="http://localhost:3001/images/logo3.png"
@@ -46,7 +60,7 @@ const Sidebar = () => {
             />
           </Link>
         </div>
-        <div className="px-[4px] mt-[158px] ">
+        <div className="px-[4px] mt-[148px] ">
           <ul>
             {allNav.map((n, i) => (
               <li key={i}>
@@ -55,8 +69,8 @@ const Sidebar = () => {
                   className={`${
                     pathname === n.path
                       ? "text-white duration-500 border-l-4 border-white ml-[-2px] "
-                      : "text-gray-500 font-bold duration-200"
-                  } px-[32px] py-[2px] flex justify-start items-center gap-[12px] hover:pl-[36px] transition-all w-full mb-6 text-xl`}
+                      : "text-gray-500  duration-200"
+                  } px-[32px] py-[2px] flex justify-start items-center gap-[12px] hover:pl-[36px] transition-all w-full mb-6 text-xl font-bold`}
                 >
                   <span>{n.icon}</span>
                   <span>{n.title}</span>
@@ -66,7 +80,7 @@ const Sidebar = () => {
           </ul>
           <div className="px-[4px] mt-[260px]">
             <li>
-              <button className="text-gray-500 font-bold duration-200 px-[32px] py-[2px] flex justify-start items-center gap-[12px] hover:pl-[36px] transition-all w-full mb-8 text-xl">
+              <button className="text-gray-500 font-bold duration-200 px-[32px] py-[2px] flex justify-start items-center gap-[12px] hover:pl-[36px] transition-all w-full mb-8 text-xl ">
                 <span>
                   <TbLogout2 />
                 </span>
@@ -75,9 +89,9 @@ const Sidebar = () => {
             </li>
           </div>
 
-          <div className="px-[4px] mt-[60px]">
+          <div className="px-[4px] mt-[60px] ">
             <li>
-              <button className="text-gray-400 font-bold duration-200 px-[32px]  flex justify-start items-center gap-[12px]  w-full text-lg">
+              <button className="text-gray-400 font-bold duration-200 px-[32px]  flex justify-start items-center gap-[12px]  w-full text-lg ">
                 <span className="text-xl">
                   <MdOutlineDateRange />
                 </span>
@@ -87,7 +101,7 @@ const Sidebar = () => {
               </button>
             </li>
             <li>
-              <button className="text-gray-400 font-bold duration-200 px-[32px] flex justify-start items-center gap-[12px]  w-full text-lg">
+              <button className="text-gray-400 font-bold duration-200 px-[32px] flex justify-start items-center gap-[12px]  w-full text-lg ">
                 <span className="text-xl">
                   <TiTime />
                 </span>
