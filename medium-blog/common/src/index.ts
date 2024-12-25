f7c9d8cc7ff40199a32da26393774e1c6b53e0e2
aditya <adityakunghadkar@gmail.com>
2024-12-25 23:59:41 +0530

    Medium-blog frontend: Signin and Signup

--- medium-blog/common/src/index.ts
+++ medium-blog/common/src/index.ts
@@ -1,28 +1,30 @@
 import z from "zod";
 
 export const signupInput = z.object({
-  username: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email(),
+  password: z.string(),
   name: z.string().optional(),
 });
 
+export type SignupType = z.infer<typeof signupInput>;
+
 export const signinInput = z.object({
-  username: z.string().email(),
-  password: z.string().min(6),
+  email: z.string().email(),
+  password: z.string(),
 });
 
-export const createBlogInput = z.object({
+export type SigninType = z.infer<typeof signinInput>;
+
+export const createPostInput = z.object({
   title: z.string(),
   content: z.string(),
 });
 
-export const updateBlogInput = z.object({
-  title: z.string(),
-  content: z.string(),
-  id: z.number(),
+export type CreatePostType = z.infer<typeof createPostInput>;
+
+export const updatePostInput = z.object({
+  title: z.string().optional(),
+  content: z.string().optional(),
 });
 
-export type SignupInput = z.infer<typeof signupInput>;
-export type SigninInput = z.infer<typeof signinInput>;
-export type CreateBlogInput = z.infer<typeof createBlogInput>;
-export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
+export type UpdatePostType = z.infer<typeof updatePostInput>;
---
+++ medium-blog/frontend/src/components/Quote.tsx
@@ -0,0 +1,20 @@
+export const Quote = () => {
+  return (
+    <div className="bg-stone-200 h-screen flex flex-col justify-center ">
+      <div className="flex justify-center">
+        <div className="max-w-lg  ">
+          <div className="text-3xl font-bold">
+            "The customer support I received was exceptional. The support team
+            went above and beyond to address my concerns"
+          </div>
+          <div className="max-w-md  text-xl font-semibold mt-2">
+            Julies Winfield
+          </div>
+          <div className="max-w-md  text-sm font-medium text-stone-500">
+            CEO | Acme Corp.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
