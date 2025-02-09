010addd004b1acb546e5d6faccab39cbb8fe2204
aditya <adityakunghadkar@gmail.com>
2025-02-09 23:59:15 +0530

    Art-X Frontend+Backend : Category form   Category controller

--- Art-X/backend/routes/dashboard/categoryRoutes.js
+++ Art-X/backend/routes/dashboard/categoryRoutes.js
@@ -4,4 +4,6 @@ const router = require("express").Router();
 
 router.post("/category-add", authMiddleware, categoryController.add_category);
 
+router.get("/category-get", authMiddleware, categoryController.get_category);
+
 module.exports = router;
