3d8ffd809dc9d6e47ad1e468aa5727640139415d
aditya <adityakunghadkar@gmail.com>
2025-03-14 23:57:45 +0530

    Art-X Frontend: Update the design of topbar and sub-topbarof the header of landing page

--- Art-X/frontend/src/components/Header.jsx
+++ Art-X/frontend/src/components/Header.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdEmail } from "react-icons/md";
 import { IoMdPhonePortrait } from "react-icons/io";
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaList, FaLock, FaUser } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+
+  const [showSidebar, setShowSidebar] = useState(true);
+  const user = true;
+
   return (
     <div className="w-full bg-white">
-      <div className="header-top bg-[#caddff] md-lg:hidden">
+      <div className="header-top bg-stone-400 md-lg:hidden">
         <div className="w-[85%] lg:w-[90%] mx-auto">
           <div className="flex w-full justify-between items-center h-[50px] text-slate-500">
             <ul className="flex justify-start items-center gap-8 font-semibold text-black">
@@ -46,7 +52,7 @@ const Header = () => {
                   </a>
                 </div>
                 <div className="flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute before:absolute before:h-[18px] before:bg-[#afafaf] before:w-[1px] before:-left-[20px]">
-                  <div className="shape flex justify-center items-center bg-[#caddff] w-[30px] h-[30px] rounded-full">
+                  <div className="shape flex justify-center items-center bg-stone-400 w-[30px] h-[30px] rounded-full">
                     <img
                       src="http://localhost:3000/images/language.png"
                       alt=""
@@ -60,6 +66,114 @@ const Header = () => {
                     <li>English</li>
                   </ul>
                 </div>
+
+                {user ? (
+                  <Link
+                    className="flex cursor-pointer justify-center items-center gap-2 text-sm text-black"
+                    to="/dashboard"
+                  >
+                    <span>
+                      {" "}
+                      <FaUser />{" "}
+                    </span>
+                    <span>Aditya Kumar </span>
+                  </Link>
+                ) : (
+                  <Link
+                    className="flex cursor-pointer justify-center items-center gap-2 text-sm text-black"
+                    to="/login"
+                  >
+                    <span>
+                      {" "}
+                      <FaLock />{" "}
+                    </span>
+                    <span>Login </span>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-white">
+        <div className="w-[85%] lg:w-[90%] mx-auto">
+          <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
+            <div className="md-lg:w-full w-3/12 md-lg:pt-4">
+              <div className="flex justify-between items-center">
+                <Link to="/">
+                  <img src="http://localhost:3000/images/logo1.png" alt="" />
+                </Link>
+                <div
+                  className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden"
+                  onClick={() => setShowSidebar(false)}
+                >
+                  <span>
+                    {" "}
+                    <FaList />{" "}
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="md:lg:w-full w-9/12">
+              <div className="flex justify-between md-lg:justify-center items-center flex-wrap pl-8">
+                <ul className="flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden">
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/" ? "text-[#059473]" : "text-slate-600"
+                      } `}
+                    >
+                      Home
+                    </Link>
+                  </li>
+
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/shop"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      } `}
+                    >
+                      Shop
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/blog"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      } `}
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/about"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      } `}
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === "/contact"
+                          ? "text-[#059473]"
+                          : "text-slate-600"
+                      } `}
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
