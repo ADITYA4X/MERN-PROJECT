654c7e0fae124b9a00fadd13ab8f87f2387ddbc4
aditya <adityakunghadkar@gmail.com>
2025-03-16 23:58:22 +0530

    Art-X Frontend: Update the design of sub-bar and side bar for mobile view of the header of landing page

--- Art-X/frontend/src/components/Header.jsx
+++ Art-X/frontend/src/components/Header.jsx
@@ -7,12 +7,16 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { Link, useLocation } from "react-router-dom";
+import { FaHeart } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Header = () => {
   const { pathname } = useLocation();
 
   const [showSidebar, setShowSidebar] = useState(true);
   const user = true;
+
+  const wishlist_count = 3;
 
   return (
     <div className="w-full bg-white">
@@ -95,14 +99,13 @@ const Header = () => {
           </div>
         </div>
       </div>
-
       <div className="w-white">
         <div className="w-[85%] lg:w-[90%] mx-auto">
           <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
             <div className="md-lg:w-full w-3/12 md-lg:pt-4">
               <div className="flex justify-between items-center">
                 <Link to="/">
-                  <img src="http://localhost:3000/images/logo1.png" alt="" />
+                  <img src="http://localhost:3000/images/logo.png" alt="" />
                 </Link>
                 <div
                   className="justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden"
@@ -121,9 +124,11 @@ const Header = () => {
                 <ul className="flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden">
                   <li>
                     <Link
-                      className={`p-2 block ${
-                        pathname === "/" ? "text-[#059473]" : "text-slate-600"
-                      } `}
+                      className={`p-2 inline-block border-b-4 ${
+                        pathname === "/"
+                          ? "text-stone-900 border-stone-900"
+                          : "text-slate-600 border-transparent"
+                      }`}
                     >
                       Home
                     </Link>
@@ -132,9 +137,9 @@ const Header = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/shop"
-                          ? "text-[#059473]"
-                          : "text-slate-600"
+                        pathname === "/"
+                          ? "text-stone-900 border-stone-900"
+                          : "text-slate-600 border-transparent"
                       } `}
                     >
                       Shop
@@ -143,9 +148,9 @@ const Header = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/blog"
-                          ? "text-[#059473]"
-                          : "text-slate-600"
+                        pathname === "/"
+                          ? "text-stone-900 border-stone-900"
+                          : "text-slate-600 border-transparent"
                       } `}
                     >
                       Blog
@@ -154,9 +159,9 @@ const Header = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/about"
-                          ? "text-[#059473]"
-                          : "text-slate-600"
+                        pathname === "/"
+                          ? "text-stone-900 border-stone-900"
+                          : "text-slate-600 border-transparent"
                       } `}
                     >
                       About Us
@@ -165,17 +170,149 @@ const Header = () => {
                   <li>
                     <Link
                       className={`p-2 block ${
-                        pathname === "/contact"
-                          ? "text-[#059473]"
-                          : "text-slate-600"
+                        pathname === "/"
+                          ? "text-stone-900 border-stone-900"
+                          : "text-slate-600 border-transparent"
                       } `}
                     >
                       Contact Us
                     </Link>
                   </li>
                 </ul>
+
+                <div className="flex md-lg:hidden justify-center items-center gap-5">
+                  <div className="flex justify-center gap-5">
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-stone-700">
+                        <FaHeart />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] ">
+                        {wishlist_count}
+                      </div>
+                    </div>
+
+                    <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
+                      <span className="text-xl text-stone-700">
+                        <FaCartShopping />
+                      </span>
+                      <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] ">
+                        {wishlist_count}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="hidden md-lg:block">
+        <div
+          onClick={() => setShowSidebar(true)}
+          className={`fixed duration-200 transition-all ${
+            showSidebar ? "invisible" : "visible"
+          } hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20 `}
+        ></div>
+
+        <div
+          className={`w-[300px] z-[9999] transition-all duration-200 fixed ${
+            showSidebar ? "-left-[300px]" : "left-0 top-0"
+          } overflow-y-auto bg-white h-screen py-6 px-8 `}
+        >
+          <div className="flex justify-start flex-col gap-6">
+            <Link to="/">
+              <img src="http://localhost:3000/images/logo.png" alt="" />
+            </Link>
+
+            <div className="flex justify-start items-center gap-10">
+              <div className="flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute ">
+                <div className="shape flex justify-center items-center bg-stone-400 w-[30px] h-[30px] rounded-full">
+                  <img src="http://localhost:3000/images/language.png" alt="" />
+                </div>
+                <span>
+                  <IoMdArrowDropdown />
+                </span>
+                <ul className="absolute invisible transition-all top-12 rounded-sm duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 group-hover:bg-black z-10">
+                  <li>Hindi</li>
+                  <li>English</li>
+                </ul>
+              </div>
+              {user ? (
+                <Link
+                  className="flex cursor-pointer justify-center items-center gap-2 text-sm text-black"
+                  to="/dashboard"
+                >
+                  <span>
+                    {" "}
+                    <FaUser />{" "}
+                  </span>
+                  <span>Aditya Kumar </span>
+                </Link>
+              ) : (
+                <Link
+                  className="flex cursor-pointer justify-center items-center gap-2 text-sm text-black"
+                  to="/login"
+                >
+                  <span>
+                    {" "}
+                    <FaLock />{" "}
+                  </span>
+                  <span>Login </span>
+                </Link>
+              )}
+            </div>
+
+            <ul className="flex flex-col justify-start items-start text-sm font-bold uppercase">
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/" ? "text-[#059473]" : "text-slate-600"
+                  } `}
+                >
+                  Home
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/shop" ? "text-[#059473]" : "text-slate-600"
+                  } `}
+                >
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/blog" ? "text-[#059473]" : "text-slate-600"
+                  } `}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/about" ? "text-[#059473]" : "text-slate-600"
+                  } `}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === "/contact"
+                      ? "text-[#059473]"
+                      : "text-slate-600"
+                  } `}
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
