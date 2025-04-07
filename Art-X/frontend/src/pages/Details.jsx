745387a21791678a134b75407bf85e63114cda9a
aditya <adityakunghadkar@gmail.com>
2025-04-07 23:51:40 +0530

    Art-X Frontend:  Update the painting details page component with Related Product component with swiper pagination

--- Art-X/frontend/src/pages/Details.jsx
+++ Art-X/frontend/src/pages/Details.jsx
@@ -15,6 +15,10 @@ import {
   FaTwitter,
 } from "react-icons/fa";
 import Reviews from "../components/Reviews";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Details = () => {
   const images = [1, 2, 3, 4, 5];
@@ -349,6 +353,74 @@ const Details = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto">
+          <h2 className="text-2xl py-8 text-slate-600">Related Products </h2>
+          <div>
+            <Swiper
+              slidesPerView="auto"
+              breakpoints={{
+                1280: {
+                  slidesPerView: 3,
+                },
+                565: {
+                  slidesPerView: 2,
+                },
+              }}
+              spaceBetween={25}
+              loop={true}
+              pagination={{
+                clickable: true,
+                el: ".custom_bullet",
+              }}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {[1, 2, 3, 4, 5].map((p, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <Link className="block">
+                      <div className="relative h-[270px]">
+                        <div className="w-full h-full">
+                          <img
+                            className="w-full h-full"
+                            src={`http://localhost:3000/images/paintings/${p}.jpg`}
+                            alt=""
+                          />
+                          <div className="absolute h-full w-full top-0 left-0 bg-[#000] opacity-25 hover:opacity-50 transition-all duration-500"></div>
+                        </div>
+                        {discount !== 0 && (
+                          <div className="flex justify-center items-center absolute text-white w-[38px] h-[38px] rounded-full bg-red-500 font-semibold text-xs left-2 top-2">
+                            {discount}%
+                          </div>
+                        )}
+                      </div>
+                      <div className="p-4 flex flex-col gap-1">
+                        <h2 className="text-slate-600 text-lg font-bold">
+                          Product Name{" "}
+                        </h2>
+                        <div className="flex justify-start items-center gap-3">
+                          <h2 className="text-lg font-bold text-slate-600">
+                            $434
+                          </h2>
+                          <div className="flex">
+                            <Rating ratings={4.5} />
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+
+          <div className="w-full flex justify-center items-center py-8">
+            <div className="custom_bullet justify-center gap-3 !w-auto"></div>
           </div>
         </div>
       </section>
