e42397d188ad8d816749bf11e1d2285ada1a8296
aditya <adityakunghadkar@gmail.com>
2025-04-30 23:56:59 +0530

    Art-X Frontend+Backend:  Update query paintings with priceQuery,skip, limit, getpainting and count Paintings

--- Art-X/backend/utiles/queryPaintings.js
+++ Art-X/backend/utiles/queryPaintings.js
@@ -23,6 +23,61 @@ class queryPaintings {
       : this.paintings;
     return this;
   };
+
+  priceQuery = () => {
+    this.paintings = this.paintings.filter(
+      (p) =>
+        (p.price >= this.query.lowPrice) & (p.price <= this.query.highPrice)
+    );
+    return this;
+  };
+  sortByPrice = () => {
+    if (this.query.sortPrice) {
+      if (this.query.sortPrice === "low-to-high") {
+        this.paintings = this.paintings.short(function (a, b) {
+          return a.price - b.price;
+        });
+      } else {
+        this.paintings = this.paintings.short(function (a, b) {
+          return b.price - a.price;
+        });
+      }
+    }
+    return this;
+  };
+
+  skip = () => {
+    let { pageNumber } = this.query;
+    const skipPage = (parseInt(pageNumber) - 1) * this.query.parPage;
+    let skipPainting = [];
+
+    for (let i = skipPage; i < this.paintings.length; i++) {
+      skipPaintings.push(this.paintings[i]);
+    }
+    this.paintings = skipProduct;
+    return this;
+  };
+
+  limit = () => {
+    let temp = [];
+    if (this.paintings.length > this.query.parPage) {
+      for (let i = 0; i < this.query.parPage; i++) {
+        temp.push(this.paintings[i]);
+      }
+    } else {
+      temp = this.paintings;
+    }
+    this.paintings = temp;
+    return this;
+  };
+
+  getPaintings = () => {
+    return this.paintings;
+  };
+
+  countPaintings = () => {
+    return this.paintings.length;
+  };
 }
 
 module.exports = queryPaintings;
